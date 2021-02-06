f886fc0534b32659479cef72eb10533ef31f6b88
Matias Olivera <matibjolivera@gmail.com>
2021-02-06 15:01:07 -0300

    delete search word

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < WooCommerceController
   ORDERS_RESOURCE = "orders/"
 
   def index
-    resource = ORDERS_RESOURCE + "search/"
+    resource = ORDERS_RESOURCE
     @result = get(resource, get_url_params)
     render "index"
   end
