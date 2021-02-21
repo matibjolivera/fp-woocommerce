32323bdc22757d1568340d437daaf1267c623117
Matias Olivera <matibjolivera@gmail.com>
2021-02-21 11:45:42 -0300

    save address data

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,8 +13,26 @@ class OrdersController < WooCommerceController
 
   def save
     orders = get_orders
-    orders.each do |order|
-      Order.find_or_create_by(reference: order["id"])
+    orders.each do |external_order|
+      order = Order.find_or_create_by(reference: external_order["id"])
+      order.billing = Billing.create(build_address(external_order["billing"]))
+      order.shipping = Shipping.create(build_address(external_order["shipping"]))
     end
+  end
+
+  private
+
+  def build_address(external_info)
+    {
+      first_name: external_info["first_name"],
+      last_name: external_info["last_name"],
+      email: external_info["email"],
+      province: external_info["state"],
+      city: external_info["city"],
+      street: external_info["address_1"],
+      extra_information: external_info["address_2"],
+      phone: external_info["phone"],
+      zip_code: external_info["postcode"]
+    }
   end
 end
